f4b20b183f3c506ad615108b7f9af967bc08a2d5
Guru Venkataraman <guru@yantrahar.com>
2026-03-26 15:22:45 -0700

    Implemented hybrid for compute+semantic

---
+++ event_registry.py
@@ -0,0 +1,21 @@
+EVENTS = {
+    "pradosham": {
+        "type": "tithi",
+        "value": "Trayodashi",
+        "temple_service": "Pradosha Rudrabhishekam"
+    },
+    "sankatahara chaturthi": {
+        "type": "tithi",
+        "value": "Chaturthi"
+    },
+    "swathi nakshatra": {
+        "type": "nakshatra",
+        "value": "Swathi"
+    },
+    "ayyappa bhajans": {
+        "type": "monthly_weekday",
+        "occurrence": "3rd Saturday",
+        "time": "5:00 PM",
+        "location": "Sri Venkateswara Temple"
+    }
+}
